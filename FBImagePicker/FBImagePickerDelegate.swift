75c880410930fe8f7b08976160b47d56493b2f81
Stephen Radford <steve228uk@gmail.com>
2017-01-31 16:37:35 +0000

    Add delegate

---
+++ FBImagePicker/FBImagePickerDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  FBImagePickerDelegate.swift
+//  FBImagePicker
+//
+//  Created by Stephen Radford on 31/01/2017.
+//  Copyright © 2017 Cocoon Development Ltd. All rights reserved.
+//
+
+import Foundation
+
+/// The official delegate of the FBImagePicker®
+protocol FBImagePickerDelegate: NSObjectProtocol {
+    
+    /// Called when the user has selected an image from the image picker.
+    ///
+    /// - Parameter image: The image the user selected
+    func fbImagePicker(imageSelected image: UIImage?)
+    
+}
