a49520a06dcf1a64edb24daf9fcb18701e9f67fd
Stephen Radford <steve228uk@gmail.com>
2017-01-31 11:54:15 +0000

    Ignore frameworks

---
+++ FBImagePicker/FBImagePickerError.swift
@@ -0,0 +1,9 @@
+//
+//  FBImagePickerError.swift
+//  FBImagePicker
+//
+//  Created by Stephen Radford on 31/01/2017.
+//  Copyright © 2017 Cocoon Development Ltd. All rights reserved.
+//
+
+import Foundation
